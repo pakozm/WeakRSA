525a53bea123ee7dd8ab74082ecdf2b88b0e62ab
Paco Zamora Martinez <pakozm@gmail.com>
2015-02-25 19:45:27 +0100

    Updated

--- execute.sh
+++ execute.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 make
+echo "GENERATING KEYS"
 ./rsa_keys
+echo "ENCRIPTING MESSAGE"
 ./rsa_encript private_key.txt < message > message.encripted
+echo "DECRIPTING MESSAGE"
 ./rsa_decript public_key.txt < message.encripted > message.decripted
+echo "CHECKING RESULT"
 if ! diff -q message message.decripted; then
     echo "Fail!"
 else
